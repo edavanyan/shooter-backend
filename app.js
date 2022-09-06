7ba7c4ede70600cb9d62f2de48ce7ee22216a1d5
edgar <edgaravanyan@gmail.com>
2022-09-06 14:48:16 +0400

    websocket

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+const index = require('./index')
+
+app.set("port", port);
+app.listen(port, function(error) {
+    if (error) {
+        console.log("error: ", error);
+    } else {
+        console.log("listening on port: " + port);
+    }
+});
+
+app.get('/name', function(req, res) {
+    res.send('qaq ker');
+    res.end();
+});
