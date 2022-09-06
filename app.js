d18df8e796fb40044c95ad661fe049e00fdb726b
edgar <edgaravanyan@gmail.com>
2022-09-06 15:20:23 +0400

    init with express

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 const index = require('./index')
+
+app.set("port", port);
+app.listen(port, function(error) {
+    if (error) {
+        console.log("error: ", error);
+    } else {
+        console.log("listening on port: " + port);
+    }
+});
+
+app.get('/name', function(req, res) {
+    res.send('qaq ker');
+    res.end();
+});
+
+index.initWebSocket(app);
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({port : process.env.PORT || 8080}, () => {
+function initWebSocket(server) {
+    const wss = new WebSocket.Server({ server }, () => {
 
-});
+    });
 
-wss.on('connection', (socket) => {
-    socket.on('message', (data) => {
-        console.log(data);
-        socket.send(data);
+    wss.on('connection', (socket) => {
+        socket.on('message', (data) => {
+            console.log(data);
+            socket.send(data);
+        })
     })
-})
 
-wss.on('listening', () => {
-    console.log("Server is now listening");
-})
+    wss.on('listening', () => {
+        console.log("Server is now listening");
+    })
+}
+
+module.exports = {
+    initWebSocket
+}
