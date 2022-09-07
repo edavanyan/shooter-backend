765317d1136cb50676b70d58974fa20c271e7f71
edgar <edgaravanyan@gmail.com>
2022-09-07 12:49:00 +0400

    move via network

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {Server} = require("ws");
 
+const connections = []
+
 function initWebSocket(server) {
     const wss = new Server({ server }, () => {
         console.log("start web server")
@@ -8,16 +10,36 @@ function initWebSocket(server) {
     console.log("init web socket: " + server);
 
     wss.on('connection', (socket) => {
+        connections.push(socket)
+
         socket.on('message', (data) => {
-            let json = JSON.parse(data.toString())
-            console.log(json);
-            socket.send(json.sender);
+            let jsonData = JSON.parse(data.toString());
+            for( var i = 0; i < connections.length; i++) { 
+                connections[i].send(data.toString());
+            }
+        })
+
+        socket.on('disconnect', (data) => {
+            connections = removeElement(connections, socket);
         })
     })
-
+    
     wss.on('listening', () => {
         console.log("Server is now listening");
     })
+}
+
+function removeElement(arr, element) {
+
+    for( var i = 0; i < arr.length; i++) { 
+    
+        if ( arr[i] === element) { 
+    
+            arr.splice(i, 1); 
+        }
+    
+    }
+    return arr;
 }
 
 module.exports = {
