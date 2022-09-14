582540883375678d651398e975a2d37955d29ec9
edgar <edgaravanyan@gmail.com>
2022-09-14 21:17:20 +0400

    position key in data

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function initWebSocket(server, game) {
                 socket.id = jsonData.id;
                 connections[jsonData.id] = socket;
 
-                jsonData.data = getSpawnPosition();
+                jsonData.data = {position : getSpawnPosition()};
 
                 getMapFromClient(jsonData.id);
             }
